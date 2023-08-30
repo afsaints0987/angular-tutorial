8949d0afdac1dbc0fa905bce862106ee2ac4b103
afsaints0987 <saints0987@gmail.com>
2023-08-30 19:42:06 +0800

    update details component, add forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -7,6 +7,12 @@ import { HousingLocation } from './housinglocation';
 export class HousingService {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
+    );
+  }
+
   housingLocationList: HousingLocation[] = [
     {
       id: 0,
